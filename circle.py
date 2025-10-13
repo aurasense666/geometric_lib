28a9cc48eea314f95ccc988ad3d9e9ba86c2da7b
Anton <nikitanikitakoskin@gmail.com>
2025-10-13 22:52:04 +0300

    Add file circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Вычисляет площадь круга с заданным радиусом.'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Вычисляет длину окружности с заданным радиусом.'''
     return 2 * math.pi * r
-
+    
